fc5791e4a93fda1dd830177877cbba10370d48be
Sal Jefferson <salailama1@gmail.com>
2023-03-26 15:33:53 -0500

    new screenshot

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
 </main>
 <footer class="blog-footer">
     <div class="sections container d-flex mt-5 pt-5 gap-4 justify-content-between">
-        <div class="left">
+        <div class="left section">
             <h3>Locations</h3>
             <?php if(is_active_sidebar('footer-left')) : ?>
             <?php dynamic_sidebar('footer-left'); ?>
             <?php endif; ?>
         </div>
-        <div class="center">
+        <div class="center section">
             <h3>Services</h3>
             <?php if(is_active_sidebar('footer-center')) : ?>
             <?php dynamic_sidebar('footer-center'); ?>
             <?php endif; ?>
         </div>
-        <div class="right">
+        <div class="right section">
             <h3>Useful Links</h3>
             <?php
         wp_nav_menu( array(
@@ -27,8 +27,9 @@
         </div>
     </div>
     <div class="text-center mt-5 copywrite">
-        <span class="me-2">&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name');?></span> |
-        <span class="ms-2"><a href="https://www.142webstudio.dev"> Built by 142 Web Studio</a></span>
+        <p class="me-2">&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name');?></p>
+        <p id="seperator"> | </p>
+        <p class="ms-2"><a href="https://www.142webstudio.dev"> Built by 142 Web Studio</a></p>
     </div>
 </footer>
 <?php wp_footer(); ?>
